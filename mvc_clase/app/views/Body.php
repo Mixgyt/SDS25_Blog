efd94f7e4cc633f10794cad0c0ac0c4d554c4a23
Mixgyt <gmix990@gmail.com>
2025-10-13 22:45:27 -0600

    add: feed dia 1

--- mvc_clase/app/views/Body.php
+++ mvc_clase/app/views/Body.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +10,11 @@
     <?php
         include "components/Navbar.php";
     ?>
+    <main class="container mt-2">
     <?php
         include ("$vista.php");
     ?>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
